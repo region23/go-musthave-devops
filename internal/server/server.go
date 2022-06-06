ab10e4647f3d166c47957bfd20cfd8f2dbbac628
Pavel Pavlenko <region23@gmail.com>
2022-06-06 23:55:51 +0300

    log

--- internal/server/server.go
+++ internal/server/server.go
@@ -111,6 +111,7 @@ func (s *Server) UpdateMetricJSON(w http.ResponseWriter, r *http.Request) {
 
 	// Если хэш не пустой, то сверяем хэши
 	if metrics.Hash != "" && metrics.Hash != "none" && s.Key != "" {
+		fmt.Println("KEY: ", s.Key)
 		var serverGeneratedHash string
 		if metrics.MType == "gauge" {
 			serverGeneratedHash = serializers.Hash(metrics.MType, metrics.ID, fmt.Sprintf("%g", *metrics.Value), s.Key)
@@ -173,6 +174,7 @@ func (s *Server) GetMetricJSON(w http.ResponseWriter, r *http.Request) {
 
 	// Если хэш не пустой, то сверяем хэши
 	if s.Key != "" {
+		fmt.Println("KEY: ", s.Key)
 		var serverGeneratedHash string
 		if metrics.MType == "gauge" {
 			serverGeneratedHash = serializers.Hash(metrics.MType, metrics.ID, fmt.Sprintf("%g", *metrics.Value), s.Key)
