131ff9b22b3618c709f3370a63284ca31f4c0188
Pavel Pavlenko <region23@gmail.com>
2022-07-05 11:41:21 +0300

    fix

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -181,7 +181,7 @@ func main() {
 
 	curMetric := new(metrics.Metric)
 
-	go getMetrics(curMetric)
+	getMetrics(curMetric)
 
 	osSigChan := make(chan os.Signal, 1)
 	signal.Notify(osSigChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
